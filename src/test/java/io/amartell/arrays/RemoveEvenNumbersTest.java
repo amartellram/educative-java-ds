41a7800c93bc3f861fa6f819c9728b1e43bdc36a
Abel Martell <amartellram@gmail.com>
2022-12-27 18:34:21 -0500

    add parameterized tests

--- src/test/java/io/amartell/arrays/RemoveEvenNumbersTest.java
+++ src/test/java/io/amartell/arrays/RemoveEvenNumbersTest.java
@@ -2,15 +2,26 @@ package io.amartell.arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class RemoveEvenNumbersTest {
 
+  private static Stream<Arguments> inputAndOutput() {
+    return Stream.of(
+        Arguments.of(new int[]{1, 2, 4, 5, 10, 6, 3}, new int[]{1, 5, 3}),
+        Arguments.of(new int[]{32, 37, 10, 40, 11, 5}, new int[]{37, 11, 5}),
+        Arguments.of(new int[]{7, 4, 9, 0, 2}, new int[]{7, 9})
+    );
+  }
 
-  @Test
-  void eliminateWithFor() {
-    int[] input = {1, 2, 4, 5, 10, 6, 3};
-    int[] output = {1, 5, 3};
+
+  @ParameterizedTest
+  @MethodSource("inputAndOutput")
+  void eliminateWithFor(int[] input, int[] output) {
 
     assertArrayEquals(output, RemoveEvenNumbers.eliminateWithFor(input));
   }
