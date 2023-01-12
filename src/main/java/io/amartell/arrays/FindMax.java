b6f95f7f1c270d2e5064e487605b497676ffe3ec
Abel Martell <amartellram@gmail.com>
2023-01-12 16:29:20 -0500

    solve challenge 11 find sum of max subarray

--- src/main/java/io/amartell/arrays/FindMax.java
+++ src/main/java/io/amartell/arrays/FindMax.java
@@ -1,8 +1,23 @@
 package io.amartell.arrays;
 
 public class FindMax {
+
   public static int findMaxSumSubArray(int[] arr) {
-    // Write - Your - Code
-    return 0;
+
+    int max = Integer.MIN_VALUE;
+
+
+    for (int i = 0; i < arr.length; i++) {
+
+      int sum = arr[i];
+      int maxSum = sum;
+      for(int j = i + 1 ; j < arr.length; j++) {
+
+        sum+= arr[j];
+        maxSum = Math.max(maxSum, sum);
+      }
+      max = Math.max(max, maxSum);
+    }
+    return max;
   }
 }
--- src/test/java/io/amartell/arrays/FindMaxTest.java
+++ src/test/java/io/amartell/arrays/FindMaxTest.java
@@ -12,11 +12,19 @@ class FindMaxTest {
 
   private static Stream<Arguments> inputAndOutput() {
     return Stream.of(
-        Arguments.of(new int[]{1, 2, 3, 4}, 11)
+        Arguments.of(new int[]{1, 7, -2, -5, 10, -1}, 11),
+        Arguments.of(new int[]{5, 1, -4, 2, 3}, 7),
+        Arguments.of(new int[]{-2, 10, 7, -5, 15, 6 }, 33),
+        Arguments.of(new int[]{-5, -13, -2, -4}, -2),
+        Arguments.of(new int[]{1, 10, -1, 11, 5, -30, -7, 20, 25, -35}, 45),
+        Arguments.of(new int[]{-7, 6, -2}, 6)
     );
   }
+
   @ParameterizedTest
   @MethodSource("inputAndOutput")
-  void findMaxSumSubArray() {
+  void findMaxSumSubArray(int[] input, int output) {
+    int result = FindMax.findMaxSumSubArray(input);
+    assertEquals(output, result);
   }
 }
